96f0f5a0af2e04ec34a9ce5a105cdc4722b2c864
johnny_visser_spie <johnny.visser@spie.com>
2020-01-19 21:12:17 +0100

    First version

---
+++ sensor.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python3
+"""
+Sensor component for Afvalinfo
+Author: Johnny Visser
+Current Version: 0.0.1  20200112 - Initial Release
+
+Description:
+  Home Assistant sensor for Afvalinfo
+
+Currently supported cities:
+- sliedrecht
+
+resources options:
+- restafval (afvalstroom 92)
+- papier    (afvalstroom 87)
+- gft       (afvalstroom 3)
+
+Example config:
+Configuration.yaml:
+  sensor:
+    - platform: afvalinfo
+      resources:                       (at least 1 required)
+        - restafval
+        - papier
+      city: sliedrecht                 (required)
+      postcode: 33361AB                (required)
+      streetnumber: 1                  (required)
+      dateformat: '%d-%m'              (optional)
+      dateonly: 1                      (optional)
+"""
+
+import voluptuous as vol
+import logging
+from datetime import datetime
+from datetime import timedelta
+from bs4 import BeautifulSoup
+import urllib.request
+import urllib.error
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_RESOURCES
+from homeassistant.util import Throttle
+from homeassistant.helpers.entity import Entity
+
+REQUIREMENTS = ["BeautifulSoup4==4.7.0"]
+
+_LOGGER = logging.getLogger(__name__)
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(hours=1)
+
+CONF_CITY = "city"
+CONF_POSTCODE = "postcode"
+CONF_STREET_NUMBER = "streetnumber"
+CONF_DATE_FORMAT = "dateformat"
+CONF_DATE_ONLY = "dateonly"
+
+SENSOR_PREFIX = "Afvalinfo "
+ATTR_LAST_UPDATE = "Last update"
+ATTR_HIDDEN = "Hidden"
+
+SENSOR_TYPES = {
+    "restafval": ["restafval", "", "mdi:recycle"],
+    "papier": ["papier", "", "mdi:recycle"],
+    "gft": ["gft", "", "mdi:recycle"],
+    "textiel": ["textiel", "", "mdi:recycle"],
+}
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_RESOURCES, default=[]): vol.All(
+            cv.ensure_list, [vol.In(SENSOR_TYPES)]
+        ),
+        vol.Required(CONF_CITY, default="sliedrecht"): cv.string,
+        vol.Required(CONF_POSTCODE, default="3361AB"): cv.string,
+        vol.Required(CONF_STREET_NUMBER, default="1"): cv.string,
+        vol.Optional(CONF_DATE_FORMAT, default="%d-%m-%Y"): cv.string,
+        vol.Optional(CONF_DATE_ONLY, default=False): cv.boolean,
+    }
+)
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    _LOGGER.debug("Setup Afvalinfo retriever")
+
+    city = config.get(CONF_CITY)
+    postcode = config.get(CONF_POSTCODE)
+    street_number = config.get(CONF_STREET_NUMBER)
+    date_format = config.get(CONF_DATE_FORMAT)
+    date_only = config.get(CONF_DATE_ONLY)
+
+    try:
+        data = AfvalinfoData(city, postcode, street_number)
+    except urllib.error.HTTPError as error:
+        _LOGGER.error(error.reason)
+        return False
+
+    entities = []
+
+    for resource in config[CONF_RESOURCES]:
+        sensor_type = resource.lower()
+
+        if sensor_type not in SENSOR_TYPES:
+            SENSOR_TYPES[sensor_type] = [sensor_type.title(), "", "mdi:recycle"]
+
+        entities.append(AfvalinfoSensor(data, sensor_type, date_format, date_only))
+
+    add_entities(entities)
+
+
+class AfvalinfoData(object):
+    def __init__(self, city, postcode, street_number):
+        self.data = None
+        self.city = city
+        self.postcode = postcode
+        self.street_number = street_number
+
+    def month_to_number(self, argument):
+        return {
+            "jan": "01",
+            "feb": "02",
+            "mrt": "03",
+            "apr": "04",
+            "mei": "05",
+            "jun": "06",
+            "jul": "07",
+            "aug": "08",
+            "sep": "09",
+            "okt": "10",
+            "nov": "11",
+            "dec": "12",
+        }[argument]
+
+    def get_date_from_afvalstroom(self, ophaaldata, afvalstroom):
+        html = ophaaldata.find(href="/afvalstroom/" + str(afvalstroom))
+        date = html.i.string[3:]
+        day = date.split(" ")[0]
+        month = self.month_to_number(date.split(" ")[1])
+        year = str(
+            datetime.today().year
+            if datetime.today().month <= int(month)
+            else datetime.today().year + 1
+        )
+        return year + "-" + month + "-" + day
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        _LOGGER.debug("Updating Waste collection dates using scraper")
+
+        try:
+            suffix_url = self.postcode + ":" + self.street_number + "/"
+            url = "https://sliedrecht.afvalinfo.nl/adres/" + suffix_url
+            req = urllib.request.Request(url=url)
+            f = urllib.request.urlopen(req)
+            html = f.read().decode("utf-8")
+
+            soup = BeautifulSoup(html, "html.parser")
+            ophaaldata = soup.find(id="ophaaldata")
+
+            waste_dict = {}
+            # find afvalstroom/3 = gft
+            waste_dict["gft"] = self.get_date_from_afvalstroom(ophaaldata, 3)
+            # find afvalstroom/7 = textiel
+            waste_dict["textiel"] = self.get_date_from_afvalstroom(ophaaldata, 7)
+            # find afvalstroom/87 = papier
+            waste_dict["papier"] = self.get_date_from_afvalstroom(ophaaldata, 87)
+            # find afvalstroom/92 = restafval
+            waste_dict["restafval"] = self.get_date_from_afvalstroom(ophaaldata, 92)
+
+            self.data = waste_dict
+        except urllib.error.URLError as exc:
+            _LOGGER.error("Error occurred while fetching data: %r", exc.reason)
+            self.data = None
+            return False
+
+
+class AfvalinfoSensor(Entity):
+    def __init__(self, data, sensor_type, date_format, date_only):
+        self.data = data
+        self.type = SENSOR_TYPES[sensor_type][0]
+        self.date_format = date_format
+        self.date_only = date_only
+        self._name = SENSOR_PREFIX + SENSOR_TYPES[sensor_type][0]
+        self._unit = SENSOR_TYPES[sensor_type][1]
+        self._icon = SENSOR_TYPES[sensor_type][2]
+        self._hidden = False
+        self._state = None
+        self._last_update = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def icon(self):
+        return self._icon
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def device_state_attributes(self):
+        return {ATTR_LAST_UPDATE: self._last_update, ATTR_HIDDEN: self._hidden}
+
+    @property
+    def unit_of_measurement(self):
+        return self._unit
+
+    def update(self):
+        self.data.update()
+        waste_data = self.data.data
+
+        _LOGGER.warning("waste_data")
+        _LOGGER.warning(waste_data)
+        _LOGGER.warning("self.type")
+        _LOGGER.warning(self.type)
+
+        try:
+            if waste_data:
+                if self.type in waste_data:
+                    today = datetime.today()
+
+                    _LOGGER.warning("today")
+                    _LOGGER.warning(today)
+
+                    collection_date = self.get_next_collection(
+                        today, waste_data, self.type
+                    )
+
+                    _LOGGER.warning("collection_date")
+                    _LOGGER.warning(collection_date)
+
+                    if collection_date:
+                        self._last_update = today.strftime("%d-%m-%Y %H:%M")
+                        date_diff = (collection_date - today.date()).days
+                        if self.date_only:
+                            if date_diff >= 0:
+                                self._state = collection_date.strftime(self.date_format)
+                        else:
+                            if date_diff >= 8:
+                                self._state = collection_date.strftime(self.date_format)
+                            elif date_diff > 1:
+                                self._state = collection_date.strftime(
+                                    "%A, " + self.date_format
+                                )
+                            elif date_diff == 1:
+                                self._state = collection_date.strftime(
+                                    "Tomorrow, " + self.date_format
+                                )
+                            elif date_diff == 0:
+                                self._state = collection_date.strftime(
+                                    "Today, " + self.date_format
+                                )
+                            else:
+                                self._state = None
+                                self._hidden = True
+                    else:
+                        self._state = None
+                        self._hidden = True
+                else:
+                    self._state = None
+                    self._hidden = True
+        except ValueError:
+            self._state = None
+            self._hidden = True
+
+    @staticmethod
+    def get_next_collection(today, waste_dict, fraction):
+        next_collection_date = None
+        collection_date = datetime.strptime(waste_dict[fraction], "%Y-%m-%d").date()
+        if collection_date >= today.date():
+            next_collection_date = collection_date
+        return next_collection_date
