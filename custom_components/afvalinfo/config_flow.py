244578d6fe78451412d67f6f564d034d6b758109
Jasper Slits <jasper@slits.nl>
2024-04-08 15:51:21 +0200

    Config flow reconfigure support

--- custom_components/afvalinfo/config_flow.py
+++ custom_components/afvalinfo/config_flow.py
@@ -3,10 +3,15 @@
 Config flow component for Afvalinfo
 Author: Jasper Slits
 """
+from typing import Any
+from collections.abc import Mapping
 
 from homeassistant.helpers.selector import selector
 from homeassistant.helpers import config_validation as cv
 from homeassistant import config_entries
+from homeassistant.config_entries import (
+    ConfigFlowResult
+)
 from .const.const import (
     _LOGGER,
     DOMAIN,
@@ -29,6 +34,44 @@ import voluptuous as vol
 
 class AfvalWijzerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
+
+    async def async_step_reconfigure(
+                    self, user_input: Mapping[str, Any] | None = None
+                        ) -> ConfigFlowResult:
+        entry = self.hass.config_entries.async_get_entry(self.context["entry_id"])
+        assert entry
+        return await self._redo_configuration(entry.data)
+
+    async def _redo_configuration(
+                    self, entry_data: Mapping[str, Any]
+                        ) -> ConfigFlowResult:
+
+        options = list(SENSOR_TYPES.keys())
+        
+        afvalinfo_schema = vol.Schema({
+        vol.Required(CONF_ID, default=entry_data[CONF_ID]): str,
+        vol.Optional(CONF_POSTCODE, default=entry_data[CONF_POSTCODE] ): str,
+        vol.Optional(CONF_STREET_NUMBER, default=entry_data[CONF_STREET_NUMBER] ): cv.positive_int,
+        vol.Optional(CONF_STREET_NUMBER_SUFFIX, default=entry_data[CONF_STREET_NUMBER_SUFFIX]): str,
+        vol.Optional(CONF_LOCATION, default=entry_data[CONF_LOCATION]): str,
+        
+        vol.Optional(CONF_DISTRICT, default=entry_data[CONF_DISTRICT]): str,
+        vol.Optional(CONF_DATE_FORMAT, default=entry_data[CONF_DATE_FORMAT]): str,
+        vol.Optional(CONF_LOCALE, default=entry_data[CONF_LOCALE]): vol.In(["nl","en"]),
+        vol.Optional(CONF_NO_TRASH_TEXT, default=entry_data[CONF_NO_TRASH_TEXT] ): str,
+        vol.Optional(CONF_DIFTAR_CODE, default=entry_data[CONF_DIFTAR_CODE]): str,
+        vol.Optional(CONF_GET_WHOLE_YEAR, default=entry_data[CONF_GET_WHOLE_YEAR]): cv.boolean,
+        vol.Required(CONF_ENABLED_SENSORS,default=entry_data[CONF_ENABLED_SENSORS]): selector({
+                "select": {
+                    "options": options,
+                    "multiple": True,
+                    "translation_key": "sensorselect"
+                    }
+            })
+        })
+        return self.async_show_form(
+                         step_id="user", data_schema=afvalinfo_schema)
+
 
     async def async_step_user(self, info):
         if info is not None:
