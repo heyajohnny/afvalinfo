f86252b38418925f922aaf1e8ef2269ab710bf13
Hmmbob <33529490+hmmbob@users.noreply.github.com>
2024-07-03 10:48:27 +0200

    Fix config entry setup warning

--- custom_components/afvalinfo/__init__.py
+++ custom_components/afvalinfo/__init__.py
@@ -1,11 +1,12 @@
 from .const.const import (_LOGGER)
 
 from homeassistant import config_entries
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+PLATFORMS = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor" ))
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     _LOGGER.debug("__init__ set up")
     return True
-
